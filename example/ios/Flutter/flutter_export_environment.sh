e318b175371fff7329693fb92d75f1ab81f69c92
Ninjagaru <13ninjagaru13@gmail.com>
2023-11-01 16:25:49 +0700

    change version intl from 0.18 to 0.17

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\gian_\Desktop\month_picker_dialog\example"
+export "FLUTTER_ROOT=/Users/hsDev/fvm/versions/3.7.11"
+export "FLUTTER_APPLICATION_PATH=/Users/hsDev/Desktop/dnoiiz/programing/project/fork/month_picker_dialog/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
